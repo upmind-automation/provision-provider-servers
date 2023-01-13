26cedfd45fee6b863423a576e58d53ee83963c09
Harry Lewis <harry@upmind.com>
2023-01-13 15:05:06 +1100

    Add LaravelServiceProvider

---
+++ src/LaravelServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Upmind\ProvisionProviders\Servers;
+
+use Upmind\ProvisionBase\Laravel\ProvisionServiceProvider;
+use Upmind\ProvisionProviders\Servers\Category as ServersCategory;
+
+class LaravelServiceProvider extends ProvisionServiceProvider
+{
+    public function boot()
+    {
+        $this->bindCategory('servers', ServersCategory::class);
+    }
+}
