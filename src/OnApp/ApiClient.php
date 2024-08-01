c237dca21c0fcb8c6d7ff8185edec1012eedefed
Harry Lewis <harry@upmind.com>
2024-08-01 17:53:41 +0100

    Update OnApp/ApiClient::getHypervisorLocation() fix 404 api error when hypervisor_group has no location set

--- src/OnApp/ApiClient.php
+++ src/OnApp/ApiClient.php
@@ -512,6 +512,10 @@ class ApiClient
         $response = $this->makeRequest("/settings/hypervisor_zones/{$hypervisorGroupId}.json");
         $locationId = $response['hypervisor_group']['location_group_id'];
 
+        if (empty($locationId)) {
+            return null;
+        }
+
         $location = $this->getLocation((int)$locationId);
 
         return "{$location['country']} ({$location['city']})";
