69fc13cb9ea649cebf4ad69059d431bac35b9387
Elena Filippova <elena.filippova@codeline.fi>
2024-11-21 12:06:43 +0200

    Init Virtfusion provider

---
+++ src/Virtfusion/ApiClient.php
@@ -0,0 +1,383 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Upmind\ProvisionProviders\Servers\Virtfusion;
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\TransferException;
+use GuzzleHttp\HandlerStack;
+use Illuminate\Support\Str;
+use Throwable;
+use Upmind\ProvisionBase\Exception\ProvisionFunctionError;
+use Upmind\ProvisionProviders\Servers\Data\CreateParams;
+use Upmind\ProvisionProviders\Servers\Data\ResizeParams;
+use Upmind\ProvisionProviders\Servers\Virtfusion\Data\Configuration;
+
+class ApiClient
+{
+    protected Configuration $configuration;
+    protected Client $client;
+
+    public function __construct(Configuration $configuration, ?HandlerStack $handler = null)
+    {
+        $this->configuration = $configuration;
+
+        $this->client = new Client([
+            'base_uri' => sprintf('https://%s', $configuration->hostname),
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-type' => 'application/json',
+                'Authorization' => 'Bearer ' . $this->configuration->api_token,
+            ],
+            'connect_timeout' => 10,
+            'timeout' => $configuration->timeout ?? 120,
+            'handler' => $handler,
+        ]);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function makeRequest(
+        string  $command,
+        ?array  $params = null,
+        ?array  $body = null,
+        ?string $method = 'GET'
+    ): ?array
+    {
+        try {
+            $requestParams = [];
+
+            if ($params) {
+                $requestParams['query'] = $params;
+            }
+
+            if ($body) {
+                $requestParams['json'] = $body;
+            }
+
+            if (str_contains($command, '/media/iso/') || $command == '/packages') {
+                $response = $this->client->request($method, '/api/v1' . $command, $requestParams);
+            } else {
+                $response = $this->client->request($method, '/api/v1/servers' . $command, $requestParams);
+            }
+            $result = $response->getBody()->getContents();
+
+            $response->getBody()->close();
+
+            if ($result === '') {
+                return null;
+            }
+
+            return $this->parseResponseData($result);
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    /**
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     */
+    private function parseResponseData(string $response): array
+    {
+        $parsedResult = json_decode($response, true);
+
+        if (!$parsedResult) {
+            throw ProvisionFunctionError::create('Unknown Provider API Error')
+                ->withData([
+                    'response' => $response,
+                ]);
+        }
+
+        return $parsedResult;
+    }
+
+
+    /**
+     * @return no-return
+     *
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    private function handleException(Throwable $e): void
+    {
+        if ($e instanceof ProvisionFunctionError) {
+            throw $e;
+        }
+
+        $errorData = [
+            'exception' => get_class($e),
+        ];
+
+        if ($e instanceof ConnectException) {
+            $errorData['connection_error'] = $e->getMessage();
+            throw ProvisionFunctionError::create('Provider API Connection error', $e)
+                ->withData($errorData);
+        }
+
+        if ($e instanceof TransferException) {
+            if ($e instanceof BadResponseException && $e->hasResponse()) {
+                $response = $e->getResponse();
+                $reason = $response->getReasonPhrase();
+                $errorData['http_code'] = $response->getStatusCode();
+                $responseBody = $response->getBody()->__toString();
+                $responseData = json_decode($responseBody, true);
+
+                $errorMessage = $responseData['errors'][0] ?? null;
+
+                if (!$errorMessage) {
+                    $errorMessage = $responseData['msg'] ?? null;
+                }
+
+                if (!$errorMessage) {
+                    $errorMessage = $responseData['message'] ?? null;
+                }
+            }
+
+            $errorMessage = sprintf('Provider API error: %s', $errorMessage ?? $reason ?? 'Unknown');
+            throw ProvisionFunctionError::create($errorMessage)
+                ->withData($errorData);
+        }
+
+        throw ProvisionFunctionError::create('Unknown Provider API Error', $e)
+            ->withData($errorData);
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function getServerInfo(string $serverId): ?array
+    {
+        $response = $this->makeRequest("/{$serverId}");
+        $data = $response['data'];
+
+        return [
+            'customer_identifier' => (int)$data['ownerId'],
+            'instance_id' => (string)($data['id'] ?? 'Unknown'),
+            'state' => (string)($data['state'] ?? 'Unknown'),
+            'suspended' => (bool)$data['suspended'],
+            'label' => $data['name'] ?? 'Unknown',
+            'hostname' => $data['hostname'] ?? 'Unknown',
+            'ip_address' => $data['vnc']['ip'],
+            'image' => ((int)$data['settings']['osTemplateInstallId'] != 0 ? $this->getImageName((int)$data['settings']['osTemplateInstallId']) : "Unknown"),
+            'memory_mb' => (int)($data['settings']['resources']['memory'] ?? 0),
+            'cpu_cores' => (int)($data['settings']['resources']['cpuCores'] ?? 0),
+            'disk_mb' => (isset($data['settings']['resources']['storage']) ? ((int)$data['settings']['resources']['storage']) : 0) * 1024,
+            'location' => $data['hypervisor']['dataDir'] ?? 'Unknown',
+            'virtualization_type' => $data['settings']['hyperv']['vendorIdValue'] ?? 'Unknown',
+            'created_at' => isset($data['created'])
+                ? Carbon::parse((string)$data['created'])->format('Y-m-d H:i:s')
+                : null,
+            'updated_at' => isset($data['updated'])
+                ? Carbon::parse((string)$data['updated'])->format('Y-m-d H:i:s')
+                : null,
+        ];
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function getImageName(int $imageId): string
+    {
+        $response = $this->makeRequest("/1/templates");
+        $data = $response['data'];
+        foreach ($data as $group) {
+            foreach ($group as $template) {
+                if ($template['id'] == $imageId) {
+                    return $template['name'] . ' ' . $template['version'];
+                }
+            }
+        }
+
+        return 'Unknown';
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function getVNC(string $serverId): array
+    {
+        $body = [
+            'action' => 'enable'
+        ];
+
+        $response = $this->makeRequest("/{$serverId}/vnc", null, $body, 'POST');
+        $vnc = $response['data']['vnc'];
+
+        return [
+            'ip_address' => $vnc['ip'],
+            'port' => $vnc['port'],
+            'password' => $vnc['password']
+        ];
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function create(CreateParams $params): string
+    {
+        if (!is_numeric($params->size)) {
+            $size = $this->getPackageId($params->size);
+        } else {
+            $size = $params->size;
+        }
+
+        $body = [
+            'userId' => $params->customer_identifier,
+            'packageId' => $size,
+            'cpuCores' => $params->cpu_cores,
+            'storage' => (int)round(($params->disk_mb) / 1024),
+            'memory' => $params->memory_mb,
+            'hypervisorId' => $this->configuration->hypervisorId,
+        ];
+
+        $response = $this->makeRequest("", null, $body, 'POST');
+
+        if (!$id = $response['data']['id']) {
+            throw ProvisionFunctionError::create('Server creation failed')
+                ->withData([
+                    'result_data' => $response,
+                ]);
+        }
+
+        if ((int)$params->image == $params->image) {
+            $image = $params->image;
+        } else {
+            $image = null;
+        }
+
+        try {
+            $body = [
+                'name' => $params->label,
+                'hostname' => $params->label,
+                'operatingSystemId' => $image,
+            ];
+
+            $this->makeRequest("/{$id}/build", null, $body, 'POST');
+        } catch (\Exception $e) {
+            throw ProvisionFunctionError::create('Server building failed')
+                ->withData([
+                    'result_data' => $response,
+                ]);
+        }
+
+        return (string)$id;
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function resetPassword(string $serverId): void
+    {
+        $body = [
+            'user' => 'root',
+        ];
+
+        $this->makeRequest("/{$serverId}/resetPassword", null, $body, 'POST');
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function suspend(string $serverId): void
+    {
+        $this->makeRequest("/$serverId}/suspend", null, null, 'POST');
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function unsuspend(string $serverId): void
+    {
+        $this->makeRequest("/$serverId}/unsuspend", null, null, 'POST');
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function power(string $serverId, string $action): void
+    {
+        $this->makeRequest("/{$serverId}/power/{$action}", null, null, 'POST');
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function destroy(string $serverId): void
+    {
+        $params = [
+            'delay' => 0,
+        ];
+
+        $this->makeRequest("/{$serverId}", $params, null, 'DELETE');
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function changePackage(string $serverId, string $size): void
+    {
+        if (!is_numeric($size)) {
+            $size = $this->getPackageId($size);
+        }
+
+        $this->makeRequest("/{$serverId}/package/{$size}", null, null, 'PUT');
+    }
+
+    /**
+     * @param $packageName
+     * @return int|null
+     * @throws Throwable
+     */
+    private function getPackageId($packageName): ?int
+    {
+        $response = $this->makeRequest("/packages");
+        $data = $response['data'];
+        foreach ($data as $package) {
+            if ($package['name'] == $packageName) {
+                return $package['id'];
+            }
+        }
+
+        throw ProvisionFunctionError::create("Package {$packageName} not found")
+            ->withData([
+                'response' => $response,
+            ]);
+    }
+}
---
+++ src/Virtfusion/Provider.php
@@ -0,0 +1,371 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Upmind\ProvisionProviders\Servers\Virtfusion;
+
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Arr;
+use Throwable;
+use Upmind\ProvisionBase\Provider\Contract\ProviderInterface;
+use Upmind\ProvisionBase\Provider\DataSet\AboutData;
+use Upmind\ProvisionProviders\Servers\Category;
+use Upmind\ProvisionProviders\Servers\Data\ChangeRootPasswordParams;
+use Upmind\ProvisionProviders\Servers\Data\CreateParams;
+use Upmind\ProvisionProviders\Servers\Data\EmptyResult;
+use Upmind\ProvisionProviders\Servers\Data\ReinstallParams;
+use Upmind\ProvisionProviders\Servers\Data\ResizeParams;
+use Upmind\ProvisionProviders\Servers\Data\ServerIdentifierParams;
+use Upmind\ProvisionProviders\Servers\Data\ServerInfoResult;
+use Upmind\ProvisionProviders\Servers\Data\ConnectionResult;
+use Upmind\ProvisionProviders\Servers\Virtfusion\Data\Configuration;
+
+class Provider extends Category implements ProviderInterface
+{
+    protected Configuration $configuration;
+    protected ApiClient|null $apiClient = null;
+
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public static function aboutProvider(): AboutData
+    {
+        return AboutData::create()
+            ->setName('Virtfusion')
+            ->setDescription('Deploy and manage Virtfusion virtual servers')
+            ->setLogoUrl('https://api.upmind.io/images/logos/provision/virtfusion-logo@2x.png');
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function create(CreateParams $params): ServerInfoResult
+    {
+        if (!Arr::has($params, 'customer_identifier')) {
+            $this->errorResult('customer_identifier field is required!');
+        }
+
+        if (!Arr::has($params, 'size')) {
+            $this->errorResult('size field is required!');
+        }
+
+        try {
+            $serverId = $this->api()->create($params);
+
+            return $this->getServerInfoResult($serverId)->setMessage('Server created successfully!');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function getInfo(ServerIdentifierParams $params): ServerInfoResult
+    {
+        try {
+            return $this->getServerInfoResult($params->instance_id)->setMessage('Server info obtained');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    protected function getServerInfoResult($serverId): ServerInfoResult
+    {
+        $info = $this->api()->getServerInfo($serverId);
+
+        return ServerInfoResult::create($info);
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function getConnection(ServerIdentifierParams $params): ConnectionResult
+    {
+        try {
+            $vnc = $this->api()->getVNC($params->instance_id);
+
+            return ConnectionResult::create()
+                ->setMessage('SSH command generated')
+                ->setType(ConnectionResult::TYPE_SSH)
+                ->setCommand(sprintf('ssh root@%s:%s', $vnc['ip_address'], $vnc['port']))
+                ->setPassword($vnc['password']);
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function changeRootPassword(ChangeRootPasswordParams $params): ServerInfoResult
+    {
+        try {
+            $this->api()->resetPassword($params->instance_id);
+
+            return $this->getServerInfoResult($params->instance_id)->setMessage('Root password has been updated');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function resize(ResizeParams $params): ServerInfoResult
+    {
+        if (!Arr::has($params, 'size')) {
+            $this->errorResult('size field is required!');
+        }
+
+        try {
+            $this->api()->changePackage($params->instance_id, $params->size);
+
+            return $this->getServerInfoResult($params->instance_id)->setMessage('Server is rebooting');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function reinstall(ReinstallParams $params): ServerInfoResult
+    {
+        $this->errorResult('Operation not supported');
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function reboot(ServerIdentifierParams $params): ServerInfoResult
+    {
+        try {
+            $info = $this->getServerInfoResult($params->instance_id);
+
+            if ($info->state == 'failed') {
+                $this->errorResult('Virtual server is not operational.');
+            }
+
+            $this->api()->power($params->instance_id, 'restart');
+
+            return $this->getServerInfoResult($params->instance_id)->setMessage('Server is rebooting');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function shutdown(ServerIdentifierParams $params): ServerInfoResult
+    {
+        try {
+            $info = $this->getServerInfoResult($params->instance_id);
+
+            if ($info->state == 'failed') {
+                $this->errorResult('Virtual server is not operational.');
+            }
+
+            $this->api()->power($params->instance_id, 'shutdown');
+
+            return $info->setMessage('Server is shutting down')->setState('Stopping');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function powerOn(ServerIdentifierParams $params): ServerInfoResult
+    {
+        try {
+            $info = $this->getServerInfoResult($params->instance_id);
+
+            if ($info->state == 'failed') {
+                $this->errorResult('Virtual server is not operational.');
+            }
+
+            $this->api()->power($params->instance_id, 'boot');
+
+            return $info->setMessage('Server is booting')->setState('Starting');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function suspend(ServerIdentifierParams $params): ServerInfoResult
+    {
+        try {
+            $info = $this->getServerInfoResult($params->instance_id);
+
+            if ($info->suspended) {
+                $this->errorResult('Virtual server already off');
+            }
+
+            $this->api()->suspend($params->instance_id);
+
+            return $info->setMessage('Server is shutting down')->setState('Stopping');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function unsuspend(ServerIdentifierParams $params): ServerInfoResult
+    {
+        try {
+            $info = $this->getServerInfoResult($params->instance_id);
+
+            if (!$info->suspended) {
+                return $info->setMessage('Virtual server already on');
+            }
+
+            $this->api()->unsuspend($params->instance_id);
+
+            return $info->setMessage('Server is booting')->setState('Stopping');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     */
+    public function attachRecoveryIso(ServerIdentifierParams $params): ServerInfoResult
+    {
+        $this->errorResult('Operation not supported');
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     */
+    public function detachRecoveryIso(ServerIdentifierParams $params): ServerInfoResult
+    {
+        $this->errorResult('Operation not supported');
+    }
+
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    public function terminate(ServerIdentifierParams $params): EmptyResult
+    {
+        try {
+            $this->getServerInfoResult($params->instance_id);
+
+            $this->api()->destroy($params->instance_id);
+
+            return EmptyResult::create()->setMessage('Server is deleting');
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+
+    /**
+     * @return ApiClient
+     */
+    protected function api(): ApiClient
+    {
+        return $this->apiClient ??= new ApiClient(
+            $this->configuration,
+            $this->getGuzzleHandlerStack()
+        );
+    }
+
+
+    /**
+     * @return no-return
+     *
+     * @throws \Upmind\ProvisionBase\Exception\ProvisionFunctionError
+     * @throws \Throwable
+     */
+    protected function handleException(Throwable $e): void
+    {
+        throw $e;
+    }
+}
